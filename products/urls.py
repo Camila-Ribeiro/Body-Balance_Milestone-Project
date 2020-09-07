e7b066ec5bd12edf7e011b69cf60c4a31b0a27b2
Camila <camilarf@gmail.com>
2020-09-07 23:12:54 +0100

    added quantity functionality, deleted and created again products folder

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shop_all_products, name='shop_products'),
-    path('<int:product_id>/', views.get_product_detail, name='product_detail'),
+    path('<int:product_id>/', views.get_product_detail, name='get_product_detail'),
 ]
