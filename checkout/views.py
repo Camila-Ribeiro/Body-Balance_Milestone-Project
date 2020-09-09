5ced2775a9ea3641e7b48c62e245f2ac4def9207
Camila <camilarf@gmail.com>
2020-09-09 17:10:06 +0100

    Added stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'product_order_form':product_order_form,
+        'stripe_public_key':'pk_test_51HPVMQEcvV2absiRNu1nkSZkj1qcnM4LAaNp2z1GMOPSZH0zGbtX2UuWIr5Ze1cs2kwHjQ4QA6qNOAdy830Y0RLX00ygGPN2eq',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
