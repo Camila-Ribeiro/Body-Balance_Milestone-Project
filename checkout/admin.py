8b7dd92b465a58ee79515e7b3dcf7fee025208bd
Camila <camilarf@gmail.com>
2020-09-14 12:45:22 +0100

    User profile - updated webhook handler to handle profiles

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,22 +12,22 @@ class ProductOrderAdminInline(admin.TabularInline):
 class ProductOrderAdmin(admin.ModelAdmin):
     inlines = (ProductOrderAdminInline,)
 
-    readonly_fields = ('order_number', 'purchase_date',
+    readonly_fields = ('order_number', 'date',
                        'delivery_cost', 'order_total',
                        'shop_total', 'original_shop_bag',
                        'stripe_pid',)
 
-    fields = ('order_number', 'user_profile' 'purchase_date', 'full_name',
+    fields = ('order_number', 'user_profile' 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county', 'delivery_cost',
               'order_total', 'shop_total', 'original_shop_bag',
               'stripe_pid',)
 
-    list_display = ('order_number', 'purchase_date', 'full_name',
+    list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
                     'shop_total',)
 
-    ordering = ('-purchase_date',)
+    ordering = ('-date',)
 
 admin.site.register(ProductOrder, ProductOrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -26,7 +26,7 @@ class ProductOrder(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    purchase_date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     shop_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
