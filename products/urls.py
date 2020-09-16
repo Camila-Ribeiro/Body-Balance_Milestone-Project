e3e592e5809b8dbcb695e167e0e5dbd3172e4eb5
Camila <camilarf@gmail.com>
2020-09-16 15:12:17 +0100

    Product Admin - fixed image field

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.shop_all_products, name='shop_products'),
     path('<int:product_id>/', views.get_product_detail, name='get_product_detail'),
-    path('add_admin/', views.add_product_admin, name='add_product_admin'),
+    path('add_product/', views.add_product_admin, name='add_product_admin'),
     path('edit_product/<int:product_id>/', views.edit_product_admin, name='edit_product_admin'),
     path('delete_product/<int:product_id>/', views.delete_product_admin, name='delete_product_admin'),
 ]
