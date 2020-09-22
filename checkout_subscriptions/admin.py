9d6ec2de12dee6656a4ffc7cdaa8bb6c5c18611c
Camila <camilarf@gmail.com>
2020-09-22 18:01:35 +0100

    Checkout_subscription = added forms and admin

--- checkout_subscriptions/admin.py
+++ checkout_subscriptions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SubscriptionOrder
+
+class SubscriptionOrderAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('date', 'order_number', 
+                       'plan_name', 'price',
+                       'stripe_pid',)
+
+    fields = ('date', 'order_number', 'plan_name', 'user_profile',
+              'full_name', 'email', 'price', 'stripe_pid',)
+            
+
+    list_display = ('date', 'order_number', 'plan_name',
+                    'price',)
+
+    ordering = ('-date',)
+
+admin.site.register(SubscriptionOrder, SubscriptionOrderAdmin)
