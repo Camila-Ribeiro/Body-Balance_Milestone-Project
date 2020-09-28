e30fa91d0a2383c813097b6885d0591490153b39
Camila <camilarf@gmail.com>
2020-09-29 00:21:00 +0100

    deployment finished

--- body_balance/settings.py
+++ body_balance/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['body-balance-milestone-project.herokuapp.com', '127.0.0.1', 'localhost', ]
+ALLOWED_HOSTS = ['body-balance-ms4.herokuapp.com', '127.0.0.1', 'localhost', ]
 
 # Application definition
 
