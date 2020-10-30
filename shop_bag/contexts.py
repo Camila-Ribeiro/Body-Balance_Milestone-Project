8de973e882c632a34377c0e5e0e8cd40ecd5bc77
Camila <camilarf@gmail.com>
2020-10-30 12:41:01 +0000

    clean up code, refactor, deleted imgs and unused code

--- shop_bag/contexts.py
+++ shop_bag/contexts.py
@@ -10,24 +10,13 @@ def bag_products(request):
     total_items = 0
     product_count = 0
     shop_bag = request.session.get('shop_bag', {})
-    get_cat = None
-    get_plan_total = None
 
     for product_id, item_data in shop_bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=product_id)
             category = product.category
-
-            #  get nutrition plan category
-            get_cat = category.category_name
             total_items += item_data * product.price
             product_count += item_data
-
-            if get_cat == 'nutrition_plan':
-                nut_price = round(product.price * Decimal
-                                  (settings.DELIVERY_PERCENTAGE / 100), 2)
-                get_plan_total = product.price
-                product_in_bag = product.product_name
             bag_items.append({
                 'product_id': product_id,
                 'product_quantity': item_data,
@@ -48,24 +37,9 @@ def bag_products(request):
                     'category': category,
                 })
 
-    #  no delivery fee if nutrition plan in shop bag
     if total_items < settings.FREE_DELIVERY:
-        if total_items == get_plan_total and get_cat == 'nutrition_plan':
-            delivery_fee = 0
-            free_delivery_delta = 0
-        elif get_cat == 'nutrition_plan' and product_in_bag:
-            delivery_fee = (total_items - get_plan_total) * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-            free_delivery_delta = 0
-            # print(delivery_fee)
-        else:
-            if get_plan_total:
-                delivery_fee = (total_items - get_plan_total)\
-                              * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-                free_delivery_delta = settings.FREE_DELIVERY - total_items
-            else:
-                delivery_fee = total_items * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-                free_delivery_delta = settings.FREE_DELIVERY - total_items
-    else:
+        delivery_fee = total_items * Decimal(settings.DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY - total_items
         delivery_fee = 0
         free_delivery_delta = 0
 
--- user_profile/views.py
+++ user_profile/views.py
@@ -55,45 +55,9 @@ def nutrition_delivery():
 
 def product_order_history(request, order_number):
     order = get_object_or_404(ProductOrder, order_number=order_number)
-    # product = get_object_or_404(Product, pk=product_id)
     profile = get_object_or_404(UserProfile, user=request.user)
     products_order = profile.orders.all()
-    # has_plan = profile.has_plan
-    nutrition_price = None
-    
-    for o in products_order:
-        # print(order.order_number)
-        for p in o.lineorders.all():
-            get_cat = p.product.category.category_name 
-            total_items = order.order_total
-            product_name = p.product.product_name
-            # print(product_name)
-           
-            if get_cat == 'nutrition_plan':
-                nutrition_price = p.product.price
-                product_name = p.product.product_name
-
-        if total_items < settings.FREE_DELIVERY:
-            if total_items == nutrition_price and get_cat == 'nutrition_plan':
-                print(get_cat)
-                delivery_cost = 0
-                print('IF')
-            elif get_cat == 'nutrition_plan' and product_name:
-                delivery_cost = (total_items - nutrition_price) * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-                print('ELIF')
-            else:
-                if product_name != product_name:
-                    print('IF ELSE')
-                    delivery_cost = (total_items - nutrition_price) * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-                    
-                else:
-                    print('ELSE ELSE')
-                    delivery_cost = total_items * Decimal(settings.DELIVERY_PERCENTAGE / 100)
-        else:
-            print('LAST ELSE')
-            delivery_cost = 0
-
-        grand_total = delivery_cost + total_items
+    nut_delivery = nutrition_delivery()
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
@@ -104,9 +68,7 @@ def product_order_history(request, order_number):
     context = {
         'order': order,
         'products_order': products_order,
-        'delivery_cost': delivery_cost,
-        'total_items': total_items,
-        'grand_total': grand_total,
+        'nut_delivery': nut_delivery,
         'from_user_profile': True,
     }
 
