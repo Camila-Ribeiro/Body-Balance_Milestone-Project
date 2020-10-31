b568886a5bfee9911985f9d9c21982553345b8b6
Camila <camilarf@gmail.com>
2020-10-31 17:22:07 +0000

    added back button and refactor views 500

--- body_balance/views.py
+++ body_balance/views.py
@@ -5,11 +5,11 @@ def handler404(request, exception):
     '''
     A 404 error handling view
     '''
-    return render(request, '404.html')
+    return render(request, '404.html', status=404)
 
 
-def handler500(request, exception):
+def handler500(request):
     '''
     A 500 error handling view
     '''
-    return render(request, '500.html')
+    return render(request, '500.html', status=500)
